67a4cd685c4e9bafb4177e93177ae5738499c7af
黄辉 <53460222@qq.com>
2019-08-15 21:14:23 +0800

    脚手架创建后使用

---
+++ src/components/01-试试.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="hh">
+<span>{{msg}}</span>
+<p>{{age}}</p>
+<p>{{gender}}</p>
+
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hahaahah',
+      age: 20,
+      gender: '男'
+    }
+  }
+}
+</script>
+// 样式
+<style>
+p{
+    color :red;
+    font-size: 30px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// 引用vue的模块
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+// 入口函数中引用相关的vue
+import haha from './components/01-试试.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  // el: '#app',h是用来渲染函数的
+  render: h => h(haha)
 }).$mount('#app')
